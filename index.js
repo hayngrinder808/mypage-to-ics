65b635d1afb2fed623090ad61c44dda6d1218fbf
Angelo Ashmore <angeloashmore@me.com>
2015-07-13 05:49:22 -1000

    Moving weekdays to static prop

--- index.js
+++ index.js
@@ -51,15 +51,18 @@ class App {
 class Shift {
   static momentFormat = "MMM D, YYYY h:mmA";
 
-  constructor(date, dayOfWeek, startTime, endTime) {
+  static weekdays = function() {
     const weekdays = moment.weekdays();
     weekdays.unshift(weekdays.pop());
+    return weekdays;
+  }
 
+  constructor(date, dayOfWeek, startTime, endTime) {
     const start = moment(`${date} ${startTime}`, Shift.momentFormat);
-    start.add(weekdays.indexOf(dayOfWeek), "days");
+    start.add(Shift.weekdays().indexOf(dayOfWeek), "days");
 
     const end = moment(`${date} ${endTime}`, Shift.momentFormat);
-    end.add(weekdays.indexOf(dayOfWeek), "days");
+    end.add(Shift.weekdays().indexOf(dayOfWeek), "days");
     if (end.isBefore(start)) end.add(1, "days");
 
     this.start = start.toDate();
