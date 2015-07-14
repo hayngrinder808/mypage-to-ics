c8b9d5d5adbe63361656e38b65459a63bb25c4a3
Angelo Ashmore <angeloashmore@me.com>
2015-07-13 15:21:49 -1000

    Embracing ES6 modules

--- mypage-to-ics/index.js
+++ mypage-to-ics/index.js
@@ -1,15 +1,4 @@
 import App from 'app';
 
-// JavaScript dependencies
-const dependencies = {
-  "moment": "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.min.js",
-  "ics": "https://cdn.rawgit.com/angeloashmore/ics-js/master/ics.es5.min.js"
-};
-
-// Load dependencies and run the app
-$.getScript(dependencies["moment"], () => {
-  $.getScript(dependencies["ics"], () => {
-    const app = new App();
-    app.execute();
-  });
-});
+const app = new App();
+app.execute();
--- mypage-to-ics/shift.js
+++ mypage-to-ics/shift.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export default class Shift {
   static momentFormat = "MMM D, YYYY h:mmA";
 
